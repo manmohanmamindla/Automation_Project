b8abae991dbd668b9deec623c9eb260813d6a35a
root <root@ip-172-31-63-14.ec2.internal>
2023-03-06 17:19:41 +0000

    first commit

--- automation.sh
+++ automation.sh
@@ -1,4 +1,8 @@
+#Make the script executible
 chmod  +x  /root/Automation_Project/automation.sh
+#switch to root user with sudo su
 sudo  su
 ./root/Automation_Project/automation.sh
+
+# or run with sudo privileges
 sudo ./root/Automation_Project/automation.sh
